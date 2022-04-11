527fed8bfa0881a3010c067fa08d5d535576f4d8
Lesha <89130863+lioshkaa@users.noreply.github.com>
2022-04-12 01:25:08 +0300

    Add files via upload

--- Movies/js/script.js
+++ Movies/js/script.js
@@ -1,6 +1,6 @@
 const modalWindow = document.querySelector(".modal");
 const buttonModal = document.querySelectorAll(".button-play");
-
+var player = document.getElementById("Youtube");
 buttonModal.forEach((item, i) => {
   item.addEventListener("click", () => {
     modalWindow.classList.add("active");
